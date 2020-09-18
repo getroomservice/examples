db695db290c729eeccc440f54df8a6d20ba26510
Flaque <evanjamesconrad@gmail.com>
2020-09-18 14:57:33 -0400

    create

---
+++ create.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ -z "$1" ]]; then
+    echo ""
+    echo "Usage: sh ./script <example>"
+    echo ""
+    exit 1
+fi
+
+git clone \
+  --depth 1 \
+  --filter=blob:none \
+  --no-checkout \
+  git@github.com:getroomservice/examples.git \
+  tmp_examples ;
+
+cd tmp_examples;
+
+if git checkout master -- $1; then
+  cd ..;
+  mv tmp_examples/$1 $1;
+  rm -rf tmp_examples;
+else 
+ echo ""
+ echo "  🚨 '$1' is not an example! 🚨"
+ echo ""
+ rm -rf tmp_examples;
+ exit 1;
+fi
