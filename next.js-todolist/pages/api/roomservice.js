bdc55a3819478d331396fc39dc0af814a2ca7f72
Flaque <evanjamesconrad@gmail.com>
2020-09-18 13:33:19 -0400

    add todolist, update package.json

---
+++ next.js-todolist/pages/api/roomservice.js
@@ -0,0 +1,29 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+const API_KEY = "nPSVHQI__RJ8Bj_Q6lOJA";
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export default async (req, res) => {
+  const body = req.body;
+  const user = "some-user-" + getRandomInt(1, 200);
+
+  const r = await fetch("https://super.roomservice.dev/provision", {
+    method: "post",
+    headers: {
+      Authorization: `Bearer: ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      user: user,
+      resources: body.resources,
+    }),
+  });
+
+  const json = await r.json();
+  res.json(json);
+};
