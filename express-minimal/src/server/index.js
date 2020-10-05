766f321f09b8e189e6abb3b8e40f22728b01f706
Flaque <evanjamesconrad@gmail.com>
2020-10-04 20:01:08 -0400

    add working express-minimal server

--- express-minimal/src/server/index.js
+++ express-minimal/src/server/index.js
@@ -2,42 +2,37 @@ const express = require("express");
 const fetch = require("node-fetch");
 const app = express();
 const port = 8080;
-
-app.use(express.static("dist"));
 app.use(express.json());
+
+// Replace this with your authorization scheme.
+function isLoggedIn(req) {
+  return true; // for the moment, we'll just let everyone in
+}
 
 const API_KEY = "HjCuI4jxFy9B9TJ0UBvau";
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 app.post("/api/roomservice", async (req, res) => {
-  const body = req.body;
-  const user = "some-user-" + getRandomInt(1, 200);
+  if (!isLoggedIn(req)) {
+    return res.send(401);
+  }
 
+  // In practice, this should be whatever user id YOU use.
+  const user = Math.random().toString(36).substr(2, 9);
+  const body = req.body;
   const r = await fetch("https://super.roomservice.dev/provision", {
-    method: "post",
+    method: "POST",
     headers: {
       Authorization: `Bearer: ${API_KEY}`,
       "Content-Type": "application/json",
     },
+
     body: JSON.stringify({
       user: user,
       resources: body.resources,
     }),
   });
 
-  if (r.status !== 200) {
-    throw new Error(
-      `The API call to Room Service failed! (${r.status}) Be sure to update the API_KEY to your own.`
-    );
-  }
-
-  const json = await r.json();
-  res.json(json);
+  return res.json(await r.json());
 });
 
 app.listen(port, () => {
