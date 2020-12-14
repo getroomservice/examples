98d9d69a14a664c48b1e57fea801abf46c74e954
Flaque <evanjamesconrad@gmail.com>
2020-12-14 10:38:12 -0800

    a little less minimal example

--- next.js-minimal/pages/index.js
+++ next.js-minimal/pages/index.js
@@ -1,11 +1,32 @@
+import { useMap } from "@roomservice/react";
+
 export default function Home() {
+  const [form, map] = useMap("myroom", "myform");
+
   return (
     <div>
-      In this example, an Auth Webhook is setup, we've hooked up a{" "}
-      <code>{`<RoomServiceProvider />`}</code> in{" "}
-      <code>{"/pages/_app.js"}</code> but we haven't created any rooms. You'll
-      want to replace the API_KEY in <code>{"/pages/api/roomservice.js"}</code>{" "}
-      before you get started.
+      <div>
+        In this example, an Auth Webhook is setup, we've hooked up a{" "}
+        <code>{`<RoomServiceProvider />`}</code> in{" "}
+        <code>{"/pages/_app.js"}</code>. You'll want to replace the API_KEY in{" "}
+        <code>{"/pages/api/roomservice.js"}</code> before you get started.
+      </div>
+
+      <label>
+        Title
+        <input
+          value={form.title}
+          onChange={(e) => map.set("title", e.target.value)}
+        />
+      </label>
+
+      <label>
+        Description
+        <input
+          value={form.description}
+          onChange={(e) => map.set("description", e.target.value)}
+        />
+      </label>
     </div>
   );
 }
