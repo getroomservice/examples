16c1f45f1e79f69fbc81243803badb23f1d14667
Flaque <evanjamesconrad@gmail.com>
2020-12-14 10:35:08 -0800

    simpler todolist

--- next.js-todolist/pages/index.js
+++ next.js-todolist/pages/index.js
@@ -1,5 +1,6 @@
 import { useList } from "@roomservice/react";
 import { useState } from "react";
+import { Container, Input, Todo } from "../components/ui";
 
 export default function List() {
   const [todos, list] = useList("lists", "todos");
@@ -17,7 +18,7 @@ export default function List() {
   }
 
   return (
-    <div className="container">
+    <Container>
       <p>
         In this example, your Auth Webhook is already setup, and we've created a
         little todo list using a Room Service{" "}
@@ -28,7 +29,7 @@ export default function List() {
 
       <h2>Todos</h2>
       <p>Press enter to add a new todo!</p>
-      <input
+      <Input
         type="text"
         value={text}
         onChange={(e) => setText(e.target.value)}
@@ -39,49 +40,12 @@ export default function List() {
         }}
       />
       {todos.map((l, i) => (
-        <p
-          className="todo"
-          key={JSON.stringify(l) + "-" + i}
-          onClick={() => onCheckOff(i)}
-        >
+        <Todo key={JSON.stringify(l) + "-" + i} onClick={() => onCheckOff(i)}>
           {l.object || l}
           {"-"}
           {i}
-        </p>
+        </Todo>
       ))}
-      <style jsx>{`
-        .container {
-          margin: 0 auto;
-          padding: 24px;
-          display: flex;
-          flex-direction: column;
-        }
-        input {
-          padding: 24px 24px;
-          border-radius: 4px;
-          border: 1px solid #cfdae2;
-          border-bottom: 2px solid #cfdae2;
-          display: flex;
-          font-size: 1em;
-          outline: none;
-          transition all 0.15s;
-          margin-bottom: 24px;
-        }
-        input:focus {
-          border-color: #90a6b6;
-          box-shadow: 10px 10px 30px #d3d5d6, -5px -5px 15px #ffffff;
-        }
-        .todo {
-          padding: 12px;
-          margin: 0;
-          border-bottom: 1px dashed #cfdae2;
-        }
-        .todo:hover {
-          background: #f8fafc;
-          border-color: #50a2dd;
-          cursor: pointer;
-        }
-      `}</style>
-    </div>
+    </Container>
   );
 }
