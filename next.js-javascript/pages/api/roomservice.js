46626870fc70e87cad721401a2a3a480fc31c019
Evan Conrad <evanjamesconrad@gmail.com>
2021-02-15 08:02:45 -0800

    Update roomservice.js

--- next.js-javascript/pages/api/roomservice.js
+++ next.js-javascript/pages/api/roomservice.js
@@ -9,7 +9,7 @@ export default async (req, res) => {
   const resources = [
     {
       object: "room",
-      room: body.room,
+      reference: body.room,
       permission: "join",
     },
   ];
