20456dc6d1b080811a656a5abaad4c85ba8f826f
Evan Conrad <evanjamesconrad@gmail.com>
2021-02-15 08:03:27 -0800

    Update roomservice.ts

--- next.js-minimal/pages/api/roomservice.ts
+++ next.js-minimal/pages/api/roomservice.ts
@@ -11,7 +11,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const resources = [
     {
       object: "room",
-      room: body.room,
+      reference: body.room,
       permission: "join",
     },
   ];
